eca706fa3c2eeb82091a46c4b598530d7c6ecc56
MaxNazarevskyi <ithefirstly@gmail.com>
2022-04-18 22:47:49 +0300

    Added talk with human

---
+++ Lesson_1/Task_3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 0;
+    int answer = 0;
+    cout << "Enter your age:" << endl;
+    cin >> age;
+    if (age > 10){
+        cout << "Do you study? (1/0): ";
+        cin >> answer;
+        if (answer == 1){
+            cout << "You are awesome!";
+        }
+        if (answer == 0){
+            cout << "Why?";
+        }
+    }
+    if (age < 10){
+        cout << "Wow, you are so big";
+    }
+    return 0;
+}
