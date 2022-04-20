d65dcb2582a426a2968bb2d2a718b43e54dcb4ee
MaxNazarevskyi <ithefirstly@gmail.com>
2022-04-20 14:41:10 +0300

    Reduced number of variables

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int motherboard = 0, videocard = 0, CPU = 0, PC_cost = 0;
+    int motherboard = 0, videocard = 0, CPU = 0, PC_cost = 0, accessoriesPC;
     float discount = 0;
     cout << "Motherboard price: " << endl;
     cin >> motherboard;
@@ -14,8 +14,9 @@ int main()
     cin >> CPU;
     cout << "Discount: ";
     cin >> discount;
-    discount = (motherboard + videocard + CPU) * (discount * 0.01);   // transformation to percent
-    PC_cost = (motherboard + videocard + CPU) - discount;
+    accessoriesPC = motherboard + videocard + CPU;
+    discount = accessoriesPC * (discount * 0.01);   // transformation to percent
+    PC_cost = accessoriesPC - discount;
     cout << "Your PC will cost: " << PC_cost;
     return 0;
 }
