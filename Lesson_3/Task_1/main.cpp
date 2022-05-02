668b19e8a70e5831071f41b2e9e45ced461fdc84
MaxNazarevskyi <ithefirstly@gmail.com>
2022-05-02 16:03:41 +0300

    Added redactor-checker array

---
+++ Lesson_3/Task_1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int finder, Size;
+    cout << "Enter array size: ";
+    cin >> Size;
+    int massive [Size];
+        for (int i = 0; i < Size; i++) {
+            cout << "Enter " << i + 1 << "th element: ";
+            cin >> massive[i];
+        }
+    cout << "What number you want to check: ";
+    cin >> finder;
+        for (int i = 0; i < Size; i++){
+            if (finder == massive[i])
+                cout << "I know that number!";
+        }
+    return 0;
+}
