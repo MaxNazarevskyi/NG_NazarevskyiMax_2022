0097491b6ce15c3275c83e3353b30e05dbf2b24f
MaxNazarevskyi <ithefirstly@gmail.com>
2022-05-02 15:04:55 +0300

    Deleted useless cycle and fixed code style

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -5,17 +5,14 @@ using namespace std;
 int main()
 {
 	int Size = 0;
-
 	cout << "Enter size: ";
 	cin >> Size;
-
 	for (int i = 0; i < Size; i++)
 	{
 		for (int j = 1; j < Size - i; j++)
 		{
 			cout << " ";
 		}
-
 		for (int j = Size - 2 * i; j <= Size; j++)
 		{
 			cout << "*";
@@ -23,11 +20,9 @@ int main()
 		cout << endl;
 	}
 	for (int i = 0; i < Size - 1; i++)
-            {
-		cout << " ";
-            }
-		for (int j = 2; j < 3; j++)
+        {
+            cout << " ";
+        }
         cout << "*";
-		cout << endl;
 	return 0;
 }
