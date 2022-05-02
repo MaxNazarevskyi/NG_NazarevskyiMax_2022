dd75cef90bb2ce26def83274446481ebbe13f01c
MaxNazarevskyi <ithefirstly@gmail.com>
2022-05-02 14:55:38 +0300

    Deleted /n and reduced number of variables

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -6,31 +6,29 @@ using namespace std;
 
 int main()
 {
-    int a, b, b1, c, D, x1, x2, sqD, x;
+    int a, b, c, D, x1, x2, x;
     cout << "a = ";
     cin >> a;
     cout << "b = ";
     cin >> b;
     cout << "c = ";
     cin >> c;
-    cout << "\n";
-    b1 = pow(b,2);
-    D = b1 - 4*a*c;
-    cout << "D = " << D << "\n";
-        if(D > 0)
-            cout << "\nTwo answers";
-        if(D == 0)
-            cout << "\nOne answer";
-        if(D < 0)
-    cout << "\nNo answer";
-    cout << "\n";
-    sqD = sqrt(D);
-    x1 = (-b + sqD) / 2*a;
-    x2 = (-b - sqD) / 2*a;
+    cout << endl;
+    D = pow(b,2) - 4*a*c;
+    cout << "D = " << D << endl;
+        if (D > 0)
+            cout << endl << "Two answers";
+        if (D == 0)
+            cout << endl << "One answer";
+        if (D < 0)
+    cout << endl << "No answer";
+    cout << endl;
+    x1 = (-b + sqrt(D)) / 2*a;
+    x2 = (-b - sqrt(D)) / 2*a;
     x = (-b) / 2*a;
         if (D>0)
-            cout << "First answer: " << x1 << "\n" << "Second answer: " << x2 <<endl;
-        if(D==0)
+            cout << "First answer: " << x1 << endl << "Second answer: " << x2 << endl;
+        if (D==0)
             cout << "Answer :" << x;
     return 0;
 }
