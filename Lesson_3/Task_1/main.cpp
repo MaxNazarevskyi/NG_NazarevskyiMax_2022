36211a85b87652c6a9a71ac871f268d177a30879
MaxNazarevskyi <ithefirstly@gmail.com>
2022-05-05 23:20:31 +0300

    Fixed code style

--- Lesson_3/Task_1/main.cpp
+++ Lesson_3/Task_1/main.cpp
@@ -8,12 +8,12 @@ int main()
     cout << "Enter array size: ";
     cin >> Size;
     int massive [Size];
-        for (int i = 0; i < Size; i++) {
+        for (int i = 0; i < Size; i++){
             cout << "Enter " << i + 1 << "th element: ";
             cin >> massive[i];
         }
-    cout << "What number you want to check: ";
-    cin >> finder;
+        cout << "What number you want to check: ";
+        cin >> finder;
         for (int i = 0; i < Size; i++){
             if (finder == massive[i])
                 cout << "I know that number!";
