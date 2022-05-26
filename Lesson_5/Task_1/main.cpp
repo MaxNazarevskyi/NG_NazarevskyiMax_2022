3751d9cdf8cfc59ae7205659d1632881138e0971
Maksim Nazarevskyi <ithefirstly@gmail.com>
2022-05-26 23:54:43 +0300

    Updated code style

--- Lesson_5/Task_1/main.cpp
+++ Lesson_5/Task_1/main.cpp
@@ -17,11 +17,10 @@ void upTree(int Size, int i)
     for (int j = 1; j < Size - i; j++)
         cout << " ";
     for (int j = Size - 2 * i; j <= Size; j++)
-		cout << "*";
+	cout << "*";
     cout << endl;
-	if (i != Size - 1)
+    if (i != Size - 1)
         upTree(Size, i + 1);
-
 }
 
 void downTree(int Size, int i)
