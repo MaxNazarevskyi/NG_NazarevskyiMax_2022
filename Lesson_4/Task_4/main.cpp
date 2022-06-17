57bb6b5a5e286e36b6229c05ca324e8802f57300
Maksim Nazarevskyi <ithefirstly@gmail.com>
2022-06-17 17:40:34 +0300

    Updated code style

--- Lesson_4/Task_4/main.cpp
+++ Lesson_4/Task_4/main.cpp
@@ -10,12 +10,11 @@ int main()
     cin.getline(str, Size);
     int words = 0;
     for (int i = 0; i < Size; i++){
-        if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            if(!(str[i+1]>='a' && str[i+1]<='z') || (str[i+1]>='A' && str[i+1]<='Z')){
+        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+            {
+            if (!(str[i+1]>='a' && str[i+1]<='z') || (str[i+1]>='A' && str[i+1]<='Z'))
             words++;
             }
-        }
     }
     cout << words;
     return 0;
